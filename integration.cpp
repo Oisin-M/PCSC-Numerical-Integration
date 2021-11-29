ec00dd2bfc75a486bffc756ab08d2e1a331714a8
majoor@INTRANET.EPFL.CH <colin.majoor@epfl.ch>
2021-11-29 16:36:08 +0100

    Initialise file, header and eigen directory

---
+++ integration.cpp
@@ -0,0 +1,17 @@
+//
+// Created by majoor@INTRANET.EPFL.CH on 29.11.21.
+//
+
+#include "integration.h"
+#include <Eigen/Dense>
+#include <Eigen/Sparse>
+#include <array>
+#include <iostream>
+#include <memory>
+
+
+
+
+int main() {
+    return 0;
+}
