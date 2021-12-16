ceb277175f8f4d98c59a5780206aa6c2cddc6795
majoor@INTRANET.EPFL.CH <colin.majoor@epfl.ch>
2021-12-16 11:50:22 +0100

    Added more text and deleted useless includes

--- tests/Tests.cpp
+++ tests/Tests.cpp
@@ -4,13 +4,10 @@
 
 #include <cmath>
 #include "../googletest/googletest/googletest/include/gtest/gtest.h"
-#include "../googletest/googletest/googletest/include/gtest/gtest-death-test.h"
 #include "../TxtReader.cpp"
 #include "../MidpointFormula.h"
 #include "../TrapezoidalRule.h"
 #include "../SimpsonsRule.h"
-#include "../DataStruct.h"
-#include "../AbstractIntegrationMethod.h"
 
 
 Eigen::VectorXcd func(double x, double y, Eigen::MatrixXcd &coeff) {
@@ -39,11 +36,12 @@ TEST(reader, death_tests) {
     ASSERT_DEATH (auto data = TxtReader("../tests/readfile_fail_D.txt").OutputData(), "Assertion `D>0' failed");
     ASSERT_DEATH (auto data = TxtReader("../tests/readfile_fail_m.txt").OutputData(), "Assertion `m>0' failed");
     ASSERT_DEATH (auto data = TxtReader("../tests/readfile_fail_l.txt").OutputData(), "Assertion `l>0' failed");
-
 }
 
 
 TEST(integrate, constant){
+
+    // Open file and solve for each method
     std::cout << "---------- INTEGRATION TESTS ----------" << std::endl << std::endl;
     std::string filename = "../tests/int_constant.txt";
     TxtReader reader = TxtReader(filename);
@@ -52,10 +50,12 @@ TEST(integrate, constant){
     auto trap_out = TrapezoidalRule(data, &func).Solve();
     auto simp_out = SimpsonsRule(data, &func).Solve();
 
+    // Expected result
     Eigen::VectorXcd expected (2);
     expected(0) = std::complex<double>(2,4);
     expected(1) = std::complex<double>(-2,8);
 
+    // All 3 methods are exact
     EXPECT_FLOAT_EQ(std::norm(expected(0)), std::norm(mid_out(0)));
     EXPECT_FLOAT_EQ(std::norm(expected(1)), std::norm(mid_out(1)));
 
@@ -66,7 +66,10 @@ TEST(integrate, constant){
     EXPECT_FLOAT_EQ(std::norm(expected(1)), std::norm(simp_out(1)));
 }
 
+
 TEST(integrate, rank3) {
+
+    // Open file and solve for each method
     std::string filename = "../tests/int_rank3.txt";
     TxtReader reader = TxtReader(filename);
     Data data = reader.OutputData();
@@ -74,10 +77,12 @@ TEST(integrate, rank3) {
     auto trap_out = TrapezoidalRule(data, &func).Solve();
     auto simp_out = SimpsonsRule(data, &func).Solve();
 
+    // Expected result
     Eigen::VectorXcd expected (2);
     expected(0) = std::complex<double>(60,-32);
     expected(1) = std::complex<double>(28,-50);
 
+    // Simpson and midpoint are exact, trapezoidal is approximate
     EXPECT_FLOAT_EQ(std::norm(expected(0)), std::norm(mid_out(0)));
     EXPECT_FLOAT_EQ(std::norm(expected(1)), std::norm(mid_out(1)));
 
@@ -90,7 +95,10 @@ TEST(integrate, rank3) {
     EXPECT_TRUE(std::imag(expected(1)) <= std::imag(trap_out(1))+0.1 && std::imag(expected(1)) >= std::imag(trap_out(1))-0.1);
 }
 
+
 TEST(integrate, rank6) {
+
+    // Open file and solve for each method
     std::string filename = "../tests/int_rank6.txt";
     TxtReader reader = TxtReader(filename);
     Data data = reader.OutputData();
@@ -98,10 +106,12 @@ TEST(integrate, rank6) {
     auto trap_out = TrapezoidalRule(data, &func).Solve();
     auto simp_out = SimpsonsRule(data, &func).Solve();
 
+    // Expected result
     Eigen::VectorXcd expected (2);
     expected(0) = std::complex<double>(7.75,7.75);
     expected(1) = std::complex<double>(7.75,7.75);
 
+    // Simpson is exact, midpoint and trapezoidal are approximate
     EXPECT_TRUE(std::real(expected(0)) <= std::real(mid_out(0))+0.1 && std::real(expected(0)) >= std::real(mid_out(0))-0.1);
     EXPECT_TRUE(std::imag(expected(0)) <= std::imag(mid_out(0))+0.1 && std::imag(expected(0)) >= std::imag(mid_out(0))-0.1);
     EXPECT_TRUE(std::real(expected(1)) <= std::real(mid_out(1))+0.1 && std::real(expected(1)) >= std::real(mid_out(1))-0.1);
@@ -116,7 +126,10 @@ TEST(integrate, rank6) {
     EXPECT_TRUE(std::imag(expected(1)) <= std::imag(trap_out(1))+0.1 && std::imag(expected(1)) >= std::imag(trap_out(1))-0.1);
 }
 
+
 TEST(integrate, rank9) {
+
+    // Open file and solve for each method
     std::string filename = "../tests/int_rank9.txt";
     TxtReader reader = TxtReader(filename);
     Data data = reader.OutputData();
@@ -124,11 +137,13 @@ TEST(integrate, rank9) {
     auto trap_out = TrapezoidalRule(data, &func).Solve();
     auto simp_out = SimpsonsRule(data, &func).Solve();
 
+    // Expected result
     Eigen::VectorXcd expected (1);
     double real = 977.0/60.0;
     double imag = -464.0/60.0;
     expected(0) = std::complex<double>(real,imag);
 
+    // All 3 methods are approximate
     EXPECT_TRUE(std::real(expected(0)) <= std::real(mid_out(0))+0.1 && std::real(expected(0)) >= std::real(mid_out(0))-0.1);
     EXPECT_TRUE(std::imag(expected(0)) <= std::imag(mid_out(0))+0.1 && std::imag(expected(0)) >= std::imag(mid_out(0))-0.1);
 
